7364cd934aac095650e957b2cbac3d9cb30577e0
Jetty2020 <kmclo@naver.com>
2021-03-09 12:17:00 +0900

    2021-3-9 leetcode #70_Climbing_Stairs failed

---
+++ leetcode/#70_Climbing_Stairs.js
@@ -0,0 +1,34 @@
+// You are climbing a staircase. It takes n steps to reach the top.
+
+// Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+// Example 1:
+
+// Input: n = 2
+// Output: 2
+// Explanation: There are two ways to climb to the top.
+// 1. 1 step + 1 step
+// 2. 2 steps
+// Example 2:
+
+// Input: n = 3
+// Output: 3
+// Explanation: There are three ways to climb to the top.
+// 1. 1 step + 1 step + 1 step
+// 2. 1 step + 2 steps
+// 3. 2 steps + 1 step
+
+let climbStairs = function (n) {
+  let a = 1,
+    b = 2,
+    next;
+
+  for (let i = 3; i <= n; i++) {
+    next = a + b;
+    a = b;
+    b = next;
+  }
+  return n === 1 ? a : b;
+};
+
+console.log(climbStairs(4));
