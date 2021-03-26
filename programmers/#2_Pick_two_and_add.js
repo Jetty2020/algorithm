48a5fa53b953d619fce249d3959e53bb3bcfec5c
Jetty2020 <kmclo@naver.com>
2021-03-26 11:04:24 +0900

    2021-3-26 programmers #4_Recommend_new_ID

--- programmers/#2_Pick_two_and_add.js
+++ programmers/#2_Pick_two_and_add.js
@@ -1,7 +1,7 @@
 // 정수 배열 numbers가 주어집니다. numbers에서 서로 다른 인덱스에 있는 두 개의 수를 뽑아 더해서 만들 수 있는 모든 수를 배열에 오름차순으로 담아 return 하도록 solution 함수를 완성해주세요.
 
 function solution(numbers) {
-  var answer = [];
+  let answer = [];
   for (let i = 0; i < numbers.length; i++) {
     for (let j = i + 1; j < numbers.length; j++) {
       let sum = numbers[i] + numbers[j];
