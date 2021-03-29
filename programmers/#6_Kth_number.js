8564afbbb02a6e5580bb72672f71b17f54f81c5b
Jetty2020 <kmclo@naver.com>
2021-03-29 12:16:23 +0900

    2021-3-29 programmers #7_Gym_suit

--- programmers/#6_Kth_number.js
+++ programmers/#6_Kth_number.js
@@ -10,7 +10,9 @@
 function solution(array, commands) {
   let answer = [];
   for (let i = 0; i < commands.length; i++) {
-    let an = array.slice(commands[i][0] - 1, commands[i][1]).sort((a, b) => {return a - b})[commands[i][2]-1];
+    let an = array.slice(commands[i][0] - 1, commands[i][1]).sort((a, b) => {
+      return a - b;
+    })[commands[i][2] - 1];
     answer.push(an);
   }
   return answer;
