298914962649d3884816dc55658f91379d7558dd
Jetty2020 <kmclo@naver.com>
2021-03-30 12:39:22 +0900

    2021-3-30 programmers #12_Array_of_dividing_numbers

---
+++ programmers/#12_Array_of_dividing_numbers.js
@@ -0,0 +1,22 @@
+// 문제 설명
+
+// array의 각 element 중 divisor로 나누어 떨어지는 값을 오름차순으로 정렬한 배열을 반환하는 함수, solution을 작성해주세요.
+// divisor로 나누어 떨어지는 element가 하나도 없다면 배열에 -1을 담아 반환하세요.
+
+function solution(arr, divisor) {
+  let answer = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % divisor === 0) {
+      answer.push(arr[i]);
+    }
+  }
+  return answer.length !== 0
+    ? answer.sort((a, b) => {
+        return a - b;
+      })
+    : [-1];
+}
+
+console.log(solution([5, 9, 7, 10], 5)); //[5, 10]
+console.log(solution([2, 36, 1, 3], 1)); //[1, 2, 3, 36]
+console.log(solution([3, 2, 6], 10)); //[-1]
