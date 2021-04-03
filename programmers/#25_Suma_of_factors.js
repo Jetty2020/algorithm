a7490f37be5deaf4eeae980cf00f34ea9d6b0864
Jetty2020 <kmclo@naver.com>
2021-04-03 11:33:43 +0900

    2021-4-3 programmers #25_Suma_of_factors

---
+++ programmers/#25_Suma_of_factors.js
@@ -0,0 +1,16 @@
+// 문제 설명
+
+// 정수 n을 입력받아 n의 약수를 모두 더한 값을 리턴하는 함수, solution을 완성해주세요.
+
+function solution(n) {
+  let answer = 0;
+  for (let i = 1; i <= n; i++) {
+    if (n % i === 0) {
+      answer += i;
+    }
+  }
+  return answer;
+}
+
+console.log(solution(12)); //28
+console.log(solution(5)); //6
