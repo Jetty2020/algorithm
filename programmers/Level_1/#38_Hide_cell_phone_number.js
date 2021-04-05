60b80f583fe4e52c4ab0098d3bb3bf696a0b6264
Jetty2020 <kmclo@naver.com>
2021-04-06 00:19:05 +0900

    2021-4-6 programmers/Level_1 #38_Hide_cell_phone_number

---
+++ programmers/Level_1/#38_Hide_cell_phone_number.js
@@ -0,0 +1,12 @@
+// 문제 설명
+
+// 프로그래머스 모바일은 개인정보 보호를 위해 고지서를 보낼 때 고객들의 전화번호의 일부를 가립니다.
+// 전화번호가 문자열 phone_number로 주어졌을 때, 
+// 전화번호의 뒷 4자리를 제외한 나머지 숫자를 전부 *으로 가린 문자열을 리턴하는 함수, solution을 완성해주세요.
+
+const solution = (num) => {
+  return '*'.repeat(num.length - 4) + num.slice(-4);
+}
+
+console.log(solution("01033334444")); //"*******4444"
+console.log(solution("027778888")); //"*****8888"
