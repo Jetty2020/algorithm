910042d2c27dcd755f4c91b893be34efd98b586c
Jetty2020 <kmclo@naver.com>
2021-04-08 12:42:28 +0900

    2021-4-8 programmers/Level_2 #1_Truck_passing_the_bridge

---
+++ programmers/Level_2/#1_Truck_passing_the_bridge.js
@@ -0,0 +1,59 @@
+// https://programmers.co.kr/learn/courses/30/lessons/42583
+
+// 문제 설명
+
+// 트럭 여러 대가 강을 가로지르는 일 차선 다리를 정해진 순으로 건너려 합니다. 모든 트럭이 다리를 건너려면 최소 몇 초가 걸리는지 알아내야 합니다. 트럭은 1초에 1만큼 움직이며, 다리 길이는 bridge_length이고 다리는 무게 weight까지 견딥니다.
+// ※ 트럭이 다리에 완전히 오르지 않은 경우, 이 트럭의 무게는 고려하지 않습니다.
+// 예를 들어, 길이가 2이고 10kg 무게를 견디는 다리가 있습니다. 무게가 [7, 4, 5, 6]kg인 트럭이 순서대로 최단 시간 안에 다리를 건너려면 다음과 같이 건너야 합니다.
+
+const solution = (bl, w, tw) => {
+  let totalW = tw.reduce((a,b)=> a+b),
+    arr = Array(bl).fill(0),
+    arrive = [0],
+    ans = 0
+    ;
+  for (; arrive.reduce((a,b) => a+b) !== totalW; ans++) {
+    let pop = arr.pop();
+    if (pop !== 0) arrive.push(pop);
+    if (arr.reduce((a,b) => a+b)+ tw[0] <= w) {
+      arr.unshift(tw.shift());
+    } else {
+      arr.unshift(0)
+    }
+
+  }
+  return ans;
+};
+
+//Better
+
+const solution = (bridge_length, weight, truck_weights) => {
+  // '다리'를 모방한 큐에 간단한 배열로 정리 : [트럭무게, 얘가 나갈 시간].
+  let time = 0, qu = [[0, 0]], weightOnBridge = 0;
+
+  // 대기 트럭, 다리를 건너는 트럭이 모두 0일 때 까지 다음 루프 반복
+  while (qu.length > 0 || truck_weights.length > 0) {
+    // 1. 현재 시간이, 큐 맨 앞의 차의 '나갈 시간'과 같다면 내보내주고,
+    //    다리 위 트럭 무게 합에서 빼준다.
+    if (qu[0][1] === time) weightOnBridge -= qu.shift()[0];
+
+    if (weightOnBridge + truck_weights[0] <= weight) {
+      // 2. 다리 위 트럭 무게 합 + 대기중인 트럭의 첫 무게가 감당 무게 이하면 
+      //    다리 위 트럭 무게 업데이트, 큐 뒤에 [트럭무게, 이 트럭이 나갈 시간] 추가.
+      weightOnBridge += truck_weights[0];
+      qu.push([truck_weights.shift(), time + bridge_length]);
+    } else {
+      // 3. 다음 트럭이 못올라오는 상황이면 얼른 큐의
+      //    첫번째 트럭이 빠지도록 그 시간으로 점프한다.
+      //    참고: if 밖에서 1 더하기 때문에 -1 해줌
+      if (qu[0]) time = qu[0][1] - 1;
+    }
+    // 시간 업데이트 해준다.
+    time++;
+  }
+  return time;
+}
+
+console.log(solution(2, 10, [7, 4, 5, 6])); //8
+console.log(solution(100, 100, [10])); //101
+console.log(solution(100, 100, [10, 10, 10, 10, 10, 10, 10, 10, 10, 10])); //
