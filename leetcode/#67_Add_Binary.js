d39668cc80884474eaed51858e4fae9032c291a0
Jetty2020 <kmclo@naver.com>
2021-04-19 23:00:27 +0900

    2021-4-19 leetcode #67_Add_Binary

---
+++ leetcode/#67_Add_Binary.js
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/add-binary/
+
+const addBinary = (a, b) => {
+  return (BigInt(`0b${a}`)+ BigInt(`0b${b}`)).toString(2);
+};
+
+console.log(addBinary(11, 1)); // '100'
+console.log(addBinary(1010, 1011)); //'10101'
