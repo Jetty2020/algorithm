336f086d94342a2a18e78d2629f23b6f40d6fb7a
Jetty2020 <kmclo@naver.com>
2021-05-04 13:46:24 +0900

    2021-5-4 leetcode #344_Reverse_String

---
+++ leetcode/#344_Reverse_String.js
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/reverse-string/
+
+const reverseString = (s) => {
+  return s.reverse();
+};
+
+console.log(reverseString(['h', 'e', 'l', 'l', 'o'])); // ["o","l","l","e","h"]
+console.log(reverseString(['H', 'a', 'n', 'n', 'a', 'h'])); // ["h","a","n","n","a","H"]
