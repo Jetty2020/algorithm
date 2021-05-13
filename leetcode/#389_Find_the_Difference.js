dfc4e3e3f6127731007625083d0234168b58fed6
Jetty2020 <kmclo@naver.com>
2021-05-13 13:44:32 +0900

    2021-5-13 leetcode #389_Find_the_Difference

---
+++ leetcode/#389_Find_the_Difference.js
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/find-the-difference/
+
+const findTheDifference = (s, t) => {
+  let ans = '',
+    sArr = s.split(''),
+    tArr = t.split('');
+  for (let i = 0; i < tArr.length; i++) {
+    if (sArr.indexOf(tArr[i]) === -1) ans += tArr[i];
+    else sArr[sArr.indexOf(tArr[i])] = 0;
+  }
+  console.log(sArr)
+  return ans;
+};
+
+console.log(findTheDifference('abcd', 'abcde')); // 'e'
+console.log(findTheDifference('', 'y')); // 'y'
+console.log(findTheDifference('a', 'aa')); // 'a'
+console.log(findTheDifference('ae', 'aea')); // 'a'
