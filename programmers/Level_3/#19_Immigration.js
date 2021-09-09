64e88df21e8530d827c4ae6c3ea28e9668b3c7fe
Jetty2020 <kmclo@naver.com>
2021-09-09 13:34:56 +0900

    2021-9-9 programers/Level3/#19_Immigration.js

---
+++ programmers/Level_3/#19_Immigration.js
@@ -0,0 +1,18 @@
+// https://programmers.co.kr/learn/courses/30/lessons/43238
+
+function solution(n, times) {
+  let max = Math.max(...times) * n;
+  let min = 1;
+
+  while (min <= max) {
+    const mid = Math.floor((max + min) / 2);
+    const sum = times.reduce((a, b) => (a += Math.floor(mid / b)), 0);
+    console.log(min, max, sum);
+    if (sum >= n) max = mid - 1;
+    else min = mid + 1;
+  }
+
+  return min;
+}
+
+console.log(solution(6, [7, 10])); // 28
