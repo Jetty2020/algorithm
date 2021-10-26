5d6cd174013fbb05e57bc642c491c397026226e6
Jetty2020 <kmclo@naver.com>
2021-10-26 17:50:51 +0900

    2021-10-26 programers/Level4/#1_stepping_stone

---
+++ programmers/Level_4/#1_stepping_stone.js
@@ -0,0 +1,33 @@
+// https://programmers.co.kr/learn/courses/30/lessons/43236
+
+function solution(distance, rocks, n) {
+  let answer = 0;
+  let min = 1;
+  let max = distance;
+  rocks.sort((a, b) => a - b);
+
+  while (min <= max) {
+    const mid = Math.floor((min + max) / 2);
+    let remove = 0;
+    let prev = 0;
+
+    for (let i = 0; i < rocks.length; i++) {
+      if (rocks[i] - prev <= mid) {
+        remove++;
+      } else {
+        prev = rocks[i];
+      }
+    }
+
+    if (remove > n) {
+      max = mid - 1;
+    } else {
+      min = mid + 1;
+      answer = Math.max(answer, min);
+    }
+  }
+
+  return answer;
+}
+
+console.log(solution(25, [2, 14, 11, 21, 17], 2)); // 4
