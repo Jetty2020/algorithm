4d0d7cdece4e233b8c7c8539c289bd712f5f4c4f
Jetty2020 <kmclo@naver.com>
2021-11-04 21:13:28 +0900

    2021-11-4 programers/Level_2(2)/#10_Expected_draw

---
+++ programmers/Level_2(2)/#10_Expected_draw.js
@@ -0,0 +1,13 @@
+// https://programmers.co.kr/learn/courses/30/lessons/12985
+
+function solution(n, a, b) {
+  let answer = 0;
+  while (a !== b) {
+    a = Math.ceil(a / 2);
+    b = Math.ceil(b / 2);
+    answer++;
+  }
+  return answer;
+}
+
+console.log(solution(8, 4, 7)); // 3
